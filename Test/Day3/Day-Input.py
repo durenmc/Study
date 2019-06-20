b389cdb2a6455fe1c8d5fc172785b68c39e65d6c
durenmc <durenmc@163.com>
2019-06-20 15:22:23 +0800

    add Day3-While

--- Test/Day3/Day-Input.py
+++ Test/Day3/Day-Input.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
+#message = input("Tell me something, and I will repeat it back to you: ")
+#print(message)
+
+#height = input("How tall are you, in inches? ")
+#height = int(height)
+#if height >= 36:
+#    print("\nYou're tall enough to ride!")
+#else:
+#   print("\nYou'll be able to ride when you're a little older.")
+
+print(4 % 3)
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print("\nThe number " + str(number) + " is even.")
+else:
+    print("\nThe number " + str(number) + " is odd.")
+
+
+
