ff434256a287ffd57e74452917120862c120c3d1
durenmc <durenmc@163.com>
2019-06-20 10:59:31 +0800

    add Day2new and Day3

--- Test/Day2/Day2-Zidian.py
+++ Test/Day2/Day2-Zidian.py
@@ -65,3 +65,85 @@ print("\n")
 for name in sorted(favorite_languages):
     print(name.title())
 print("\n")
+
+for language in set(favorite_languages.values()):
+    print(language.title())
+
+aliens = []
+# 创建30个绿色的外星人
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+# 显示前五个外星人
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+# 显示创建了多少个外星人
+print("Total number of aliens: " + str(len(aliens)))
+
+# 创建一个用于存储外星人的空列表
+aliens = []
+# 创建30个绿色的外星人
+for alien_number in range (0,30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+# 显示前五个外星人
+for alien in aliens[0:5]:
+    print(alien)
+print("...")
+
+favorite_languages = {
+'jen': ['python', 'ruby'],
+'sarah': ['c'],
+'edward': ['ruby', 'go'],
+'phil': ['python', 'haskell'],
+}
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+        },
+    }
+
+for username,user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
+
+people = []
+one = {'name':'zhangsan','age':30}
+two = {'name':'xiaosi','age':35}
+people.append(one)
+people.append(two)
+print(people)
+
+for user in people:
+    for key,value in user.items():
+        print(key.title()+":"+str(value))
