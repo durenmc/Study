df328c4a7a207ff4d876f16dbc2f9872a593a65a
durenmc <durenmc@163.com>
2019-07-10 17:30:41 +0800

    add GameDay1

---
+++ Game/alien_invsion.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
+    #设置背景色bg_color = (230,230,230)
+
+    #开始游戏的主循环
+    while True:
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #每次循环都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
+
